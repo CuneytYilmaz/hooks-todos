895c9a2b11171be12a49a3ba2f18f2ff907f9f4e
CuneytYilmaz <cuneyilmaz@gmail.com>
2020-01-18 23:50:25 +0300

    Removing Todos reducer / REMOVE_TODO

--- src/reducer.js
+++ src/reducer.js
@@ -4,10 +4,17 @@ export default function todosReducer (state, action) {
             const toggledTodos = state.todos.map(t => t.id === action.payload.id
                 ? { ...action.payload, complete: !action.payload.complete }
                 : t)
-                
+
             return {
                 ...state,
                 todos: toggledTodos
+            }
+        case 'REMOVE_TODO' :
+            const filteredTodos = state.todos.filter(t => t.id !== action.payload.id)
+
+            return {
+                ...state,
+                todos: filteredTodos
             }
         default :
             return state
